ce02ae582fdc8c6ab34453d7f2b2fa9220572fc0
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-07-13 15:48:02 +0530

    Time: 64 ms (10.67%), Space: 8.3 MB (10.27%) - LeetHub

---
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+        string ans = "";
+        
+        for(int i = 0; i<s.size(); i++){
+            int index = i;
+            int c = k;
+            while(c--){
+                if(index >= s.size()){
+                    index = 0;
+                }
+                index++;
+            }
+            if(index >= s.size()) index = 0;
+            ans += s[index];
+        }
+        return ans;
+    }
+};
