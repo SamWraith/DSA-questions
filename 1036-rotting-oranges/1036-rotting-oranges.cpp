2467f5be8cdff61b90d989cf0b679f2895a0bcd5
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-08-01 17:33:07 +0530

    Time: 3 ms (84.17%), Space: 16.2 MB (52.34%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -4,8 +4,9 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int vis[n][m];
+        // {{row, col}, time}
         queue<pair<pair<int, int>, int>> q;
-        // get all rotten oranges
+        // find the initial rotten oranges
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(grid[i][j] == 2){
@@ -20,6 +21,7 @@ public:
         int tm = 0;
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
+        // bfs
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
@@ -29,13 +31,14 @@ public:
             for(int i = 0; i<4; i++){
                 int nrow = r + drow[i];
                 int ncol = c + dcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-                && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
+                if(nrow >= 0 && nrow<n && ncol >=0 && ncol < m && 
+                vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
                     vis[nrow][ncol] = 2;
                     q.push({{nrow, ncol}, t+1});
                 }
             }
         }
+
         // check if any fresh oranges are left
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
