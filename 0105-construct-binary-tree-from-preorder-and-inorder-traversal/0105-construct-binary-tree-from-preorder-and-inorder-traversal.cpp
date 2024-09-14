47b136b53e02b7e1ec62a16d2f13c7863adf6457
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-09-14 12:53:19 +0530

    Time: 13 ms (53.24%), Space: 27.4 MB (56.7%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,30 +11,27 @@
  */
 class Solution {
 public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i<inorder.size(); i++)
+            mp[inorder[i]] = i;
+        return buildTree(preorder, 0, preorder.size() - 1,
+                         inorder, 0, inorder.size() - 1, mp);
+    }
     TreeNode* buildTree(vector<int> &preorder, int preStart, int preEnd, 
                         vector<int> &inorder, int inStart, int inEnd,
-                                         unordered_map<int, int> &inmap)
+                           unordered_map<int, int> &mp)
     {
-        if( preStart > preEnd || inStart > inEnd ){
-            return NULL;
-        }
-
+        if(preStart > preEnd || inStart > inEnd) return NULL;
         TreeNode* root = new TreeNode(preorder[preStart]);
-        int inRoot = inmap[root->val];
+        int inRoot = mp[root->val];
         int numsLeft = inRoot - inStart;
-        root->left = buildTree(preorder, preStart + 1, preStart + numsLeft,
-                                inorder, inStart, inRoot - 1, inmap);
+        root->left = buildTree(preorder, preStart + 1, preStart + numsLeft, 
+                                inorder, inStart, inRoot - 1, mp);
         root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd,
-                                inorder, inRoot + 1, inEnd, inmap);
+                                inorder, inRoot + 1, inEnd, mp);
         return root;
     }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> inmap;
-        for(int i = 0; i<inorder.size(); i++)
-            inmap[inorder[i]] = i;
-        return buildTree(preorder, 0, preorder.size() - 1,
-                                    inorder, 0, inorder.size() - 1, inmap);
-        
-    }
-    
+                        
+                           
 };
