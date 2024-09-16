c3b6ef62b1c70211b9df2ccac5c07cd6875b1d75
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-09-16 14:46:36 +0530

    Time: 4 ms (32.14%), Space: 9.3 MB (88.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -28,12 +28,27 @@ private:
         }
         return dp[n - 1];
     }
+    int space(int n, vector<int> &nums){
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i = 1; i<n; i++){
+            int take = nums[i];
+            if(i > 1)
+                take += prev2;
+            int nottake = 0 + prev;
+            int curr = max(take, nottake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         // return solve(n - 1, nums);
         // vector<int> dp(n, -1);
         // return mem(n - 1, nums, dp);
-        return tab(n, nums);
+        // return tab(n, nums);
+        return space(n, nums);
     }
 };
