9ff9d4fbdd572c8581eb10af60fdafa9f7289f64
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-09-16 22:45:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (75.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -31,11 +31,30 @@ private:
         }
         return dp[m-1][n-1];
     }
+    int space(int m, int n){
+        vector<int> prev(n, 0);
+        prev[0] = 1;
+        for(int i = 0; i<m; i++){
+            vector<int> curr(n, 0);
+            for(int j = 0; j<n; j++){
+                if(i == 0 && j == 0) curr[j] = 1;
+                else{
+                    int up = 0, left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
 public:
     int uniquePaths(int m, int n) {
         // return solve(m - 1, n-1);
         // vector<vector<int>> dp(m, vector<int> (n, -1));
         // return mem(m - 1, n - 1, dp);
-        return tab(m, n);
+        // return tab(m, n);
+        return space(m, n);
     }
 };
