218a28590b156ef311ee557e41dfe44c2d166946
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-09-18 15:14:27 +0530

    Time: 4 ms (74.33%), Space: 11.7 MB (59.05%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -14,10 +14,25 @@ private:
         int diag = triangle[i][j] + mem(i + 1, j + 1, triangle, dp);
         return dp[i][j] = min(down, diag);
     }
+    int tab(vector<vector<int>> &triangle){
+        int n = triangle.size();
+        vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size() - 1].size(), -1));
+        for(int i = 0; i<triangle[n - 1].size(); i++)
+            dp[n - 1][i] = triangle[n - 1][i];
+        for(int i = n - 2; i>= 0; i--){
+            for(int j = i; j>=0; j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diag);
+            }
+        }
+        return dp[0][0];
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         // return solve(0, 0, triangle);
-        vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size() - 1].size(), -1));
-        return mem(0, 0, triangle, dp);
+        // vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size() - 1].size(), -1));
+        // return mem(0, 0, triangle, dp);
+        return tab(triangle);
     }
 };
