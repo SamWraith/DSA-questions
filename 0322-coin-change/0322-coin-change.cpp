27dce6c8e377f721c299b8b4ab02881978540daf
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2024-09-27 20:11:09 +0530

    Time: 36 ms (55.35%), Space: 47.1 MB (39.5%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -23,15 +23,36 @@ private:
             take = 1 + mem(i, t - coins[i], coins, dp);
         return dp[i][t] = min(take, notTake);
     }
+    int tab(int n, int target, vector<int> &coins){
+        vector<vector<int>> dp(n, vector<int>(target+1, 0));
+        for(int t = 0; t <= target; t++){
+            if(t % coins[0] == 0)
+                dp[0][t] = t/coins[0];
+            else dp[0][t] = 1e9;
+        }
+        for(int i = 1; i<n; i++){
+            for(int t = 0; t<=target; t++){
+                int notTake = 0 + dp[i - 1][t];
+                int take = INT_MAX;
+                if(t >= coins[i])
+                    take = 1 + dp[i][t - coins[i]];
+                dp[i][t] = min(take, notTake);
+            }
+        }
+        int ans = dp[n-1][target];
+        if(ans >= 1e9) return -1;
+        else return ans;
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         // return solve(n - 1, amount, coins);
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans =  mem(n - 1, amount, coins, dp);
-        if(ans >= 1e9) return -1;
-        return ans;
-
+        // vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        // int ans =  mem(n - 1, amount, coins, dp);
+        // if(ans >= 1e9) return -1;
+        // return ans;
+        
+        return tab(n, amount, coins);
 
 
 
