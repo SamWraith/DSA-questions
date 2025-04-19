21ce0a702116abe9122a40e059256596b75ad4cd
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2025-04-19 22:31:40 +0530

    Time: 76 ms (8.69%), Space: 60.5 MB (38.74%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        // nums[i] + nums[j] == nums[j] + nums[i], so the condition that i < j
-        // degrades to i != j and we can sort the array.
-        ranges::sort(nums);
-        return countLess(nums, upper) - countLess(nums, lower - 1);
-    }
-
-private:
-    long countLess(const vector<int>& nums, int sum) {
-        long res = 0;
-        for (int i = 0, j = nums.size() - 1; i < j; ++i) {
-            while (i < j && nums[i] + nums[j] > sum)
-                --j;
-            res += j - i;
+    long long lower_bound(vector<int>& nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
-        return res;
+        return low;
+    }
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        long long ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            // Assume we have picked nums[i] as the first pair element.
+
+            // `low` indicates the number of possible pairs with sum < lower.
+            int low =
+                lower_bound(nums, i + 1, nums.size() - 1, lower - nums[i]);
+
+            // `high` indicates the number of possible pairs with sum <= upper.
+            int high =
+                lower_bound(nums, i + 1, nums.size() - 1, upper - nums[i] + 1);
+
+            // Their difference gives the number of elements with sum in the
+            // given range.
+            ans += 1LL * (high - low);
+        }
+        return ans;
     }
 };
