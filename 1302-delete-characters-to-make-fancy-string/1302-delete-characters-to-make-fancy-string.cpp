b9554b1ab061664ab9bd321f5923ca52a188a940
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2025-07-21 11:44:55 +0530

    Time: 31 ms (46.51%), Space: 44.5 MB (67.47%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        string ans;
-        for (const char c : s)
-            if (ans.length() < 2 || ans.back() != c || ans[ans.size() - 2] != c)
-                ans.push_back(c);
+        char prev = s[0];
+        int frequency = 1;
+        string ans = "";
+        ans.push_back(s[0]);
+
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == prev) {
+                frequency++;
+            } else {
+                prev = s[i];
+                frequency = 1;
+            }
+
+            if (frequency < 3) ans.push_back(s[i]);
+        }
+
         return ans;
     }
 };
