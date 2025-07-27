4dfd1a5a6cdb7d2d2e5943e31af530928658d489
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2025-07-27 20:10:43 +0530

    Time: 0 ms (100%), Space: 12.1 MB (50.25%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
-        int res = 0;  // number of peaks and valleys
+        int res = 0;  
         int n = nums.size();
         for (int i = 1; i < n - 1; ++i) {
             if (nums[i] == nums[i - 1]) {
-                // deduplication
                 continue;
             }
-            int left = 0;  // left side possibly unequal neighboring
-                           // corresponding state
+            int left = 0;
             for (int j = i - 1; j >= 0; --j) {
                 if (nums[j] > nums[i]) {
                     left = 1;
@@ -19,8 +17,7 @@ public:
                     break;
                 }
             }
-            int right = 0;  // right side possibly unequal neighboring
-                            // corresponding state
+            int right = 0;
             for (int j = i + 1; j < n; ++j) {
                 if (nums[j] > nums[i]) {
                     right = 1;
@@ -31,7 +28,6 @@ public:
                 }
             }
             if (left == right && left != 0) {
-                // at this time, index i is part of a peak or valley.
                 ++res;
             }
         }
