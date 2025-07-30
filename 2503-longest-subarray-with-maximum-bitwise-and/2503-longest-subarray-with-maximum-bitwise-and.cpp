4cd811e73a34916384bfa0002a5a2eb9c46ca400
Samir Nandan Pandey <96676347+SamWraith@users.noreply.github.com>
2025-07-30 23:01:04 +0530

    Time: 0 ms (100%), Space: 86.2 MB (7.99%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int ans = 0;
-        int maxIndex = 0;
-        int sameNumLength = 0;
+        int maxVal = 0, ans = 0, currentStreak = 0;
 
-        for (int i = 0; i < nums.size(); ++i)
-            if (nums[i] == nums[maxIndex]) {
-                ans = max(ans, ++sameNumLength);
-            } else if (nums[i] > nums[maxIndex]) {
-                maxIndex = i;
-                sameNumLength = 1;
-                ans = 1;
-            } else {
-                sameNumLength = 0;
+        for (int num : nums) {
+            if (maxVal < num) {
+                maxVal = num;
+                ans = currentStreak = 0;
             }
 
+            if (maxVal == num) {
+                currentStreak++;
+            } else {
+                currentStreak = 0;
+            }
+
+            ans = max(ans, currentStreak);
+        }
         return ans;
     }
 };
